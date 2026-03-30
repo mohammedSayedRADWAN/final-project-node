4c7d8985d138612f164bf9531a36f0a0d52e20ab
Amany Mahmoud <amany234324@gmail.com>
2026-03-30 21:59:02 +0200

    implement

--- project/app.js
+++ project/app.js
@@ -1,50 +1,25 @@
-// // import express from 'express';
+import express from 'express';
 // // import { connectDB } from './MongoDB/DBConnection.js';
-// import productRoutes from './routes/ProductRoute.js';
+ import productRoutes from './routes/ProductRoute.js';
 // // import orderRoutes from './routes/order.routes.js';
-// import { errorHandler } from './middleware/ErrorMiddleware.js';
-// import paymentRoute from "./routes/paymentRoute.js";
-//   const app = express();
+ import { errorHandler } from './middleware/ErrorMiddleware.js';
+ import paymentRoute from "./routes/paymentRoute.js";
+  const app = express();
 
 
-// app.use(express.json());
+ app.use(express.json());
 
 // connectDB();
-// app.use("/api/payment", paymentRoute);
-// app.use('/products', productRoutes);
+app.use("/api/payment", paymentRoute);
+app.use('/products', productRoutes);
 // // app.use('/orders', orderRoutes);
 
 // // Error handler
-// app.use(errorHandler);
+app.use(errorHandler);
 
 // app.listen(3000, () => {
 //     console.log('Server running on port 3000');
 // });
-import express from 'express';
-// import { connectDB } from './MongoDB/DBConnection.js';
-import productRoutes from './routes/ProductRoute.js';
-// import orderRoutes from './routes/order.routes.js';
- import usereRoutes from './routes/user.routes.js';
-
-import { errorHandler } from './middleware/ErrorMiddleware.js';
-import paymentRoute from "./routes/paymentRoute.js";
-
-const app = express();
-
-// Middleware
-app.use(express.json());
-
-// Database
-// connectDB();
-
-// Routes
-app.use("/api/payment", paymentRoute);
-app.use('/products', productRoutes);
-// app.use('/orders', orderRoutes);
-app.use('/users', usereRoutes);
-
-// Error handler
-app.use(errorHandler);
 
 
 // Export app
