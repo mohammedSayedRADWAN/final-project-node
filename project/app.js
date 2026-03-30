3c3391e1e605081dc431594becad4db0a2f146fe
Amany Mahmoud <amany234324@gmail.com>
2026-03-30 19:45:12 +0200

    update app.js and package.json

--- project/app.js
+++ project/app.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import { connectDB } from './MongoDB/DBConnection.js';
+// import express from 'express';
+// import { connectDB } from './MongoDB/DBConnection.js';
 import productRoutes from './routes/ProductRoute.js';
-// import orderRoutes from './routes/OrderRoutes.js';
+import orderRoutes from './routes/OrderRoutes.js';
 import { errorHandler } from './middleware/ErrorMiddleware.js';
-
+import paymentRoute from "./routes/paymentRoute.js";
 const app = express();
 
 
 app.use(express.json());
 
 connectDB();
-
+app.use("/api/payment", paymentRoute);
 app.use('/products', productRoutes);
 // app.use('/orders', orderRoutes);
 
@@ -20,5 +20,3 @@ app.use(errorHandler);
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
-
-export { app };
